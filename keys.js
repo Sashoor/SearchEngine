76959b0420ae2a752dcce919efb2c2756e4a9d33
Dev machine <saeedashoor.dev@gmail.com>
2021-12-20 11:53:43 -0800

    working on SSR & API

---
+++ keys.js
@@ -0,0 +1,5 @@
+//API Key
+const API_KEY ="AIzaSyDpGKRCGIMNgV8Gyiuh4bxCmn6sjQHhXVo";
+
+//Context Key
+export const CONTEXT_KEY ="6df86510db68bbd94";
--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,17 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import { API_KEY, CONTEXT_KEY } from "../keys";
 
 
-function Search() {
+function Search({results}) 
+{ console.log(results)
     return (
         <div>
             <Head>
                <title>Search Results</title>         
             </Head>
 
-            {/* Header */}
+           
             <Header />
             {/* Search Results */}
 
@@ -19,3 +21,14 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+    const useDummyData = false;
+
+    const data = await fetch (`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`).then((response) => response.json());
+    return {
+        props: {
+          results: data,
+        },
+      };
+}
