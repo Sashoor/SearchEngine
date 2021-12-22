8f51ae86bdbc562289ad7bf7443e05cac9abb09a
Dev machine <saeedashoor.dev@gmail.com>
2021-12-22 14:16:16 -0800

    working on searchresults

--- pages/search.js
+++ pages/search.js
@@ -1,42 +1,41 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import Header from "../components/Header";
+import SearchResults from "../components/SearchResults";
 import Response from "../Response";
 
+function Search({ results }) {
+  const router = useRouter();
 
-
-function Search({results}) {
-const router = useRouter();
-
-{ console.log(results)
+  {
+    console.log(results);
     return (
-        <div>
-            <Head>
-            <title>{router.query.term} - Google Search</title>  
-               <link rel="icon" href="/favicon.ico" />
-            </Head>
+      <div>
+        <Head>
+          <title>{router.query.term} - Google Search</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-           
-            <Header />
-            {/* Search Results */}
-
-
-        </div>
-    )
-}
+        <Header />
+        <SearchResults results={results} />
+      </div>
+    );
+  }
 }
 export default Search;
 
 export async function getServerSideProps(context) {
-    const useDummyData = true;
-    const startIndex = context.query.start || "0";
+  const useDummyData = true;
+  const startIndex = context.query.start || "0";
 
-
-
-    const data = useDummyData ? Response : await fetch (`https://www.googleapis.com/customsearch/v1?key=AIzaSyD4sbQoBg5g1bN39XmJRRlGbV1jLwNi_1E&cx=6df86510db68bbd94&q=${context.query.term}&start=${startIndex}`).then((response) => response.json());
-    return {
-        props: {
-          results: data,
-        },
-      };
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=AIzaSyD4sbQoBg5g1bN39XmJRRlGbV1jLwNi_1E&cx=6df86510db68bbd94&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
