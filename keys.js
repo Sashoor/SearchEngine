c235fa19b25dfc3f237532c086a83b6e13de8747
Dev machine <saeedashoor.dev@gmail.com>
2021-12-22 12:57:07 -0800

    added dummy query to save quota limit

--- keys.js
+++ keys.js
@@ -1,5 +1,10 @@
-//API Key
-const API_KEY ="AIzaSyDpGKRCGIMNgV8Gyiuh4bxCmn6sjQHhXVo";
 
+// https://developers.google.com/custom-search/v1/using_rest
+//API Key
+const API_KEY ="AIzaSyD4sbQoBg5g1bN39XmJRRlGbV1jLwNi_1E";
+
+
+
+// https://cse.google.com/cse/create/new
 //Context Key
 export const CONTEXT_KEY ="6df86510db68bbd94";
--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,19 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
-import { API_KEY, CONTEXT_KEY } from "../keys";
+import Response from "../Response";
 
 
-function Search({results}) 
+
+function Search({results}) {
+const router = useRouter();
+
 { console.log(results)
     return (
         <div>
             <Head>
-               <title>Search Results</title>         
+            <title>{router.query.term} - Google Search</title>  
+               <link rel="icon" href="/favicon.ico" />
             </Head>
 
            
@@ -19,13 +24,16 @@ function Search({results})
         </div>
     )
 }
-
+}
 export default Search;
 
 export async function getServerSideProps(context) {
-    const useDummyData = false;
+    const useDummyData = true;
+    const startIndex = context.query.start || "0";
 
-    const data = await fetch (`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`).then((response) => response.json());
+
+
+    const data = useDummyData ? Response : await fetch (`https://www.googleapis.com/customsearch/v1?key=AIzaSyD4sbQoBg5g1bN39XmJRRlGbV1jLwNi_1E&cx=6df86510db68bbd94&q=${context.query.term}&start=${startIndex}`).then((response) => response.json());
     return {
         props: {
           results: data,
