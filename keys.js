042349d4bd1aa4812a8c027748db3ed7a416e3d5
Dev machine <saeedashoor.dev@gmail.com>
2021-12-25 08:46:37 -0800

    added env file

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 
 // https://developers.google.com/custom-search/v1/using_rest
 //API Key
-export const API_KEY ="AIzaSyCFPUGaAl2TcsGVHXvsYqtZ_cLhepGLmeU";
+export const API_KEY = "AIzaSyAR0O6bGkyzXrLEBU2vHKGhXmfr0lQ8djw";
 
 
 
 // https://cse.google.com/cse/create/new
 //Context Key
-export const CONTEXT_KEY ="6df86510db68bbd94";
+export const CONTEXT_KEY = "19a8f33208a57391f"; 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 module.exports = {
+  env: {
+    API_KEY: 'AIzaSyAR0O6bGkyzXrLEBU2vHKGhXmfr0lQ8djw',
+    CONTEXT_KEY: '19a8f33208a57391f'
+  },
+}
+
+module.exports = {
   reactStrictMode: true,
    
   images: {
     domains: ["assets.stickpng.com"],
   },
 }
+
+
